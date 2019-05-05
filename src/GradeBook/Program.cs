390df2f5f16dc420437698e35cf46716770519c3
Alessandro Vincenzi <alessandro84.vincenzi@gmail.com>
2019-05-05 18:51:03 +0200

    Average score

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -6,7 +7,19 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            if (args.Length > 0)
+                Console.WriteLine($"Hello, {args[0]}!");
+            else
+                Console.WriteLine($"Hello!");
+
+            var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
+ 
+            var result = 0.0;
+            foreach(var number in grades)
+                result += number;
+            result /= grades.Count;
+
+            Console.WriteLine($"The average grade is {result:N2}");
+         }
     }
 }
